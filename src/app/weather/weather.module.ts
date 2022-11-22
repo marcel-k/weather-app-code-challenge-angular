04ecb1d573302b5d85b23ecc970f9892a19a9534
Marcel van de Kamp <--global>
2022-11-22 17:00:23 +0100

    gebruik edit location fab in weather page / module

--- src/app/weather/weather.module.ts
+++ src/app/weather/weather.module.ts
@@ -1,11 +1,14 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
-import { WeatherRoutingModule } from './weather-routing.module';
-import { WeatherComponent } from './weather.component';
+import { SharedModule } from '../shared/shared.module';
 import { CurrentWeatherCardComponent } from './current-weather-card/current-weather-card.component';
 import { DayForecastWeatherCardComponent } from './day-forecast-weather-card/day-forecast-weather-card.component';
-import { ThreeDayForecastWeatherCardComponent } from './three-day-forecast-weather-card/three-day-forecast-weather-card.component';
+import {
+  ThreeDayForecastWeatherCardComponent,
+} from './three-day-forecast-weather-card/three-day-forecast-weather-card.component';
+import { WeatherRoutingModule } from './weather-routing.module';
+import { WeatherComponent } from './weather.component';
 
 
 @NgModule({
@@ -17,6 +20,7 @@ import { ThreeDayForecastWeatherCardComponent } from './three-day-forecast-weath
   ],
   imports: [
     CommonModule,
+    SharedModule,
     WeatherRoutingModule
   ]
 })
