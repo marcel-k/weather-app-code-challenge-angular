0c31d0f55d2061be7aa5802e1c9895925f09c800
Marcel van de Kamp <--global>
2022-11-23 15:12:27 +0100

    sidenav close button

--- src/app/core/layout/navigation/navigation.component.ts
+++ src/app/core/layout/navigation/navigation.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'wa-navigation',
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./navigation.component.css']
 })
 export class NavigationComponent {
+  @Output() closeSidenav: EventEmitter<void> = new EventEmitter();
 
+  constructor(){}
+
+  onMenuCloseClick() {
+    this.closeSidenav.emit();
+  }
 }
