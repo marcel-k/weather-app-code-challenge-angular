e739776c86b801ce628d81d005538b08ce554cd7
Marcel van de Kamp <--global>
2022-11-23 11:50:45 +0100

    app container component skeleton

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,31 +1,37 @@
-import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
-import { ToolbarComponent } from './layout/toolbar/toolbar.component';
-import { NavigationComponent } from './layout/navigation/navigation.component';
+import { NgModule, Optional, SkipSelf } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+import { ContainerComponent } from './layout/container/container.component';
 import { ContentComponent } from './layout/content/content.component';
 import { FooterComponent } from './layout/footer/footer.component';
+import { NavigationComponent } from './layout/navigation/navigation.component';
+import { ToolbarComponent } from './layout/toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
     ToolbarComponent,
     NavigationComponent,
     ContentComponent,
-    FooterComponent
+    FooterComponent,
+    ContainerComponent
   ],
   imports: [
     CommonModule,
     MatToolbarModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatSidenavModule
   ],
   exports: [
     ToolbarComponent,
     NavigationComponent,
     ContentComponent,
-    FooterComponent
+    FooterComponent,
+    ContainerComponent
   ]
 })
 export class CoreModule {
