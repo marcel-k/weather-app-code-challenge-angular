99c086d45250418d7284ad5fa74a54496626c6f2
Marcel van de Kamp <--global>
2022-12-01 12:40:26 +0100

    mapping json --> response --> model

--- src/app/core/weather.service.ts
+++ src/app/core/weather.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { WeatherModel } from './weather.model';
+import { WeatherResponse } from './weather.response';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,17 @@ import { Injectable } from '@angular/core';
 export class WeatherService {
 
   constructor() { }
+
+
+  private mapJsonToResponse(json: any): WeatherResponse[] {
+    const response = json.map((item: any) => new WeatherResponse().deserialize(item));
+
+    return response;
+  }
+
+  private mapResponseToModel(response: WeatherResponse): WeatherModel {
+    const model = new WeatherModel().mapResponse(response);
+
+    return model;
+  }
 }
